37cacbd439ef52232c11a46dfdfc4dd9f239f7d3
carpenterzoe <carpenterzoe@outlook.com>
2024-06-23 10:37:56 +0800

    feat: version control

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +11,12 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  // 接口版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
