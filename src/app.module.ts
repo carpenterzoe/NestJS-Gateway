db262bab4b2dec7660f8e63450904bbee0ca49fa
carpenterzoe <carpenterzoe@outlook.com>
2024-11-17 13:35:13 +0800

    feat: AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { redisStore }  from 'cache-manager-redis-yet'
 import { getConfig } from './utils';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -20,8 +23,17 @@ import { getConfig } from './utils';
       ignoreEnvFile: true,
       isGlobal: true, // 全局注册后，在业务模块就可以直接使用了
       load: [getConfig],  // load 的 getConfig 是函数
-    }), UserModule],
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule { }
