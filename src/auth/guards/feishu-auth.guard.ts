823f2d91fe21797e5e41d09769af89be99b06fde
carpenterzoe <carpenterzoe@outlook.com>
2024-11-17 09:13:57 +0800

    feat: 路由守卫

---
+++ src/auth/guards/feishu-auth.guard.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+// 因为 FeishuAuthGuard 已经继承了通用的 AuthGuard
+// 验证逻辑在 FeishuStrategy 实现了，所以并没有额外的代码出现
+// 如果有其他的逻辑则需要对不同的方法进行重写已完成需求。
+export class FeishuAuthGuard extends AuthGuard('feishu') { }
